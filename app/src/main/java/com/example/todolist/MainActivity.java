ffd5e74fbae44b5b19639e388afbd81d9e3ec3fc
stevenheroo <stevenfianu99@gmail.com>
2021-06-30 23:53:52 +0000

    Front Design development

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,14 +2,30 @@ package com.example.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton addTaskFloatBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
+
+        addTaskFloatBar = findViewById(R.id.floating_BTN);
+        addTaskFloatBar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int itemID = v.getId();
+
+        if (itemID == R.id.floating_BTN) {
+            startActivity(new Intent(this, AddTask.class));
+        }
     }
 }
